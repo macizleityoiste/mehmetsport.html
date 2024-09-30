d887b10c1c44b5e95d966c5d4bdcacd26a9a9f3b
macizleityoiste <mehmet12wswsws@gmail.com>
2024-09-30 23:17:40 +0300

    Update and rename proxy.php to mac.php

---
+++ mac.php
@@ -0,0 +1,58 @@
+<?php
+// Şifrelenmiş URL'leri tanımlayın
+$channels = [
+    [
+        'name' => 'Beşiktaş',
+        'url' => 'aHR0cHM6Ly93d3cub2hhLnRvL29oYS10di9wbGF5LzE0NTM2OTIzMTQyMzc3MWI1OTUyODk=',
+        'status' => 'CANLI',
+        'date' => '90 Dakika'
+    ],
+    [
+        'name' => 'BeinSports1 - Yedek Yayın 1',
+        'url' => 'aHR0cHM6Ly93d3cub2hhLnRvL29oYS10di9wbGF5LzE0NTM2OTIzMTQyMzc3MWI1OTUyODk=',
+        'status' => 'CANLI',
+        'date' => '7/24'
+    ],
+    [
+        'name' => 'BeinSports1 - Yedek Yayın 2',
+        'url' => 'aHR0cHM6Ly93d3cub2hhLnRvL29oYS10di9wbGF5LzIxMTM0NjIzOTg0M2RkZWRlOGZhYmM=',
+        'status' => 'CANLI',
+        'date' => '7/24'
+    ],
+    [
+        'name' => 'BeinSports1 - Yedek Yayın 3',
+        'url' => 'aHR0cHM6Ly93d3cub2hhLnRvL29oYS10di9wbGF5LzI0MDM0MjUwMzE4NzNhNDMwMGQxZTQ=',
+        'status' => 'CANLI',
+        'date' => '7/24'
+    ],
+    [
+        'name' => 'BeinSports 2 - Yedek Yayın 1',
+        'url' => 'aHR0cHM6Ly93d3cub2hhLnRvL29oYS10di9wbGF5LzM2OTQ2NjI0NzU2NTVmZmNiYTZiNDc=',
+        'status' => 'CANLI',
+        'date' => '7/24'
+    ],
+    [
+        'name' => 'BeinSports 3 - Yedek Yayın 1',
+        'url' => 'aHR0cHM6Ly93d3cub2hhLnRvL29oYS10di9wbGF5LzkzNjkwODI3NDlmNmE4NmY4NTBjMQ==',
+        'status' => 'CANLI',
+        'date' => '7/24'
+    ],
+    [
+        'name' => 'BeinSports 4 - Yedek Yayın 1',
+        'url' => 'aHR0cHM6Ly93d3cub2hhLnRvL29oYS10di9wbGF5LzE2MTk1MzQ2M2VhMWU4YmFmYmFkY2E=',
+        'status' => 'CANLI',
+        'date' => '7/24'
+    ],
+    [
+        'name' => 'S-Sport - Yedek Yayın 1',
+        'url' => 'aHR0cHM6Ly93d3cub2hhLnRvL29oYS10di9wbGF5LzkzNjkwODI3NDlmNmE4NmY4NTBjMQ==',
+        'status' => 'CANLI',
+        'date' => '7/24'
+    ],
+    [
+        'name' => 'S-Sport 2 - Yedek Yayın 1',
+        'url' => 'aHR0cHM6Ly93d3cub2hhLnRvL29oYS10di9wbGF5LzkzNjkwODI3NDlmNmE4NmY4NTBjMQ==',
+        'status' => 'CANLI',
+        'date' => '7/24'
+    ]
+];
