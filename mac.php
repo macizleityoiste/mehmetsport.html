9e11c3cf4d431e9c23ee21ee58255e39fca21100
macizleityoiste <mehmet12wswsws@gmail.com>
2024-10-01 18:31:15 +0300

    Update mac.php

--- mac.php
+++ mac.php
@@ -1,61 +1,8 @@
 <?php
-$matches = [
-    [
-        'name' => 'Beşiktaş',
-        'url' => 'aHR0cHM6Ly93d3cub2hhLnRvL29oYS10di9wbGF5LzE0NTM2OTIzMTQyMzc3MWI1OTUyODk=',
-        'status' => 'CANLI',
-        'date' => '90 Dakika'
-    ],
-    [
-        'name' => 'BeinSports1 - Yedek Yayın 1',
-        'url' => 'aHR0cHM6Ly93d3cub2hhLnRvL29oYS10di9wbGF5LzE0NTM2OTIzMTQyMzc3MWI1OTUyODk=',
-        'status' => 'CANLI',
-        'date' => '7/24'
-    ],
-    [
-        'name' => 'BeinSports1 - Yedek Yayın 2',
-        'url' => 'aHR0cHM6Ly93d3cub2hhLnRvL29oYS10di9wbGF5LzIxMTM0NjIzOTg0M2RkZWRlOGZhYmM=',
-        'status' => 'CANLI',
-        'date' => '7/24'
-    ],
-    [
-        'name' => 'BeinSports1 - Yedek Yayın 3',
-        'url' => 'aHR0cHM6Ly93d3cub2hhLnRvL29oYS10di9wbGF5LzI0MDM0MjUwMzE4NzNhNDMwMGQxZTQ=',
-        'status' => 'CANLI',
-        'date' => '7/24'
-    ],
-    [
-        'name' => 'BeinSports 2 - Yedek Yayın 1',
-        'url' => 'aHR0cHM6Ly93d3cub2hhLnRvL29oYS10di9wbGF5LzM2OTQ2NjI0NzU2NTVmZmNiYTZiNDc=',
-        'status' => 'CANLI',
-        'date' => '7/24'
-    ],
-    [
-        'name' => 'BeinSports 3 - Yedek Yayın 1',
-        'url' => 'aHR0cHM6Ly93d3cub2hhLnRvL29oYS10di9wbGF5LzkzNjkwODI3NDlmNmE4NmY4NTBjMQ==',
-        'status' => 'CANLI',
-        'date' => '7/24'
-    ],
-    [
-        'name' => 'BeinSports 4 - Yedek Yayın 1',
-        'url' => 'aHR0cHM6Ly93d3cub2hhLnRvL29oYS10di9wbGF5LzE2MTk1MzQ2M2VhMWU4YmFmYmFkY2E=',
-        'status' => 'CANLI',
-        'date' => '7/24'
-    ],
-    [
-        'name' => 'S-Sport - Yedek Yayın 1',
-        'url' => 'aHR0cHM6Ly93d3cub2hhLnRvL29oYS10di9wbGF5LzkzNjkwODI3NDlmNmE4NmY4NTBjMQ==',
-        'status' => 'CANLI',
-        'date' => '7/24'
-    ],
-    [
-        'name' => 'S-Sport 2 - Yedek Yayın 1',
-        'url' => 'aHR0cHM6Ly93d3cub2hhLnRvL29oYS10di9wbGF5LzkzNjkwODI3NDlmNmE4NmY4NTBjMQ==',
-        'status' => 'CANLI',
-        'date' => '7/24'
-    ]
+$links = [
+    ['name' => 'MehmetSport 1', 'url' => 'https://ozweb.duckdns.org/cdn/getrok/1.m3u8?v=aHR0c...', 'status' => 'CANLI', 'date' => '7/24'],
+    ['name' => 'MehmetSport 2', 'url' => 'https://ozweb.duckdns.org/cdn/getrok/2.m3u8?v=aHR0c...', 'status' => 'CANLI', 'date' => '7/24'],
+    ['name' => 'MehmetSport 3', 'url' => 'https://ozweb.duckdns.org/cdn/getrok/3.m3u8?v=aHR0c...', 'status' => 'CANLI', 'date' => '7/24'],
+    // Diğer linkler burada...
 ];
-
-header('Content-Type: application/json');
-echo json_encode($matches);
-?>
+echo json_encode($links);
