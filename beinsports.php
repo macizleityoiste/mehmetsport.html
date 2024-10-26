8a6a0531e4b5354e92da28962e4754979da6529b
macizleityoiste <mehmet12wswsws@gmail.com>
2024-10-26 23:40:27 +0300

    Update beinsports.php

--- beinsports.php
+++ beinsports.php
@@ -1,22 +1,26 @@
 <?php
-// beinsports.php
-
 header('Content-Type: application/json');
 
+// Örnek BeIN Sports kanal verileri
 $channels = [
     [
-        'name' => 'BeinSports1 - Yedek Yayın 1',
-        'url' => 'https://varsports.zortstream.shop/601/index.m3u8',
-        'status' => 'CANLI',
-        'date' => 'Maç Zamanı Aktif'
+        "name" => "BeIN Sports 1",
+        "url" => "https://ozweb.duckdns.org/cdn/getrok/1.m3u8?v=aHR0cHM6Ly93d3cub2hhLnRvL29oYS10di9wbGF5LzE1OTc4NjE2NDI0ZDJkMTFlMjU4YmY=",
+        "status" => "CANLI",
+        "date" => date("Y-m-d H:i:s")
     ],
     [
-        'name' => 'BeinSports1 - Yedek Yayın 2',
-        'url' => 'https://ozweb.duckdns.org/cdn/getrok/1.m3u8?v=aHR0cHM6Ly93d3cub2hhLnRvL29oYS10di9wbGF5LzIxMTM0NjIzOTg0M2RkZWRlOGZhYmM=',
-        'status' => 'CANLI',
-        'date' => '7/24'
+        "name" => "BeIN Sports 2",
+        "url" => "https://ozweb.duckdns.org/cdn/getrok/1.m3u8?v=aHR0cHM6Ly93d3cub2hhLnRvL29oYS10di9wbGF5LzM4MzA2OTEzMTY2ZWY3ZjQ4YzE5YmY=",
+        "status" => "CANLI",
+        "date" => date("Y-m-d H:i:s")
     ],
-   
+    [
+        "name" => "BeIN Sports 3",
+        "url" => "https://ozweb.duckdns.org/cdn/getrok/1.m3u8?v=aHR0cHM6Ly93d3cub2hhLnRvL29oYS10di9wbGF5LzE0ODk0MzA3NTQ0YWY3ODQwYmM1NWU=",
+        "status" => "CANLI",
+        "date" => date("Y-m-d H:i:s")
+    ]
 ];
 
 echo json_encode($channels);
